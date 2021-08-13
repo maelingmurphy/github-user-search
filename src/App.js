c31ad01a71ba438d862f6143bd7a4c07bc9c94e4
Maeling <maelingmurphy@users.noreply.github.com>
2021-08-13 11:16:55 -0400

    Get API data when form submits

--- src/App.js
+++ src/App.js
@@ -7,18 +7,42 @@ function App() {
   // Set state variable (userInput) to store user search form input value
   const [userInput, setUserInput] = useState("");
 
-  // Update state based on input field changes
+  // Set state variable (userSearchResults) to store array of results from API call
+  const [userSearchResults, setUserSearchResults] = useState([]);
+
+  // Update userInput state based on input field changes
   const handleChange = (event) => {
     const userInputValue = event.target.value;
     setUserInput(userInputValue);
-    console.log("User input:", userInput);
+  }
+
+  // Get data from GitHub Search API upon form submit 
+  const getUserData = async(event) => {
+    event.preventDefault();
+
+    try {
+      // Make GET request to API with userInput data
+      const response = await fetch(`https://api.github.com/search/users?q=${userInput}`);
+      // Throw error if response is not successful (not ok)
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      // Convert response object to JSON
+      const userSearchData = await response.json();
+      console.log('User search data:', userSearchData);
+      // Save search results to state (userSearchResults)
+      setUserSearchResults(userSearchData);
+
+    } catch(error) {
+      // Set search response boolean variable to false to display correct message
+      console.log("error: ", error); 
+    }
   }
 
   return (
     <div>
       <Header />
-      <SearchForm onChange={handleChange} value={userInput} />
-      <h2>{userInput}</h2>
+      <SearchForm onChange={handleChange} value={userInput} onSubmit={getUserData} />
     </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function SearchForm({onChange, value}) {
+function SearchForm({onChange, value, onSubmit}) {
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <label htmlFor="username">Enter username</label>
                 <input type="text" id="username" placeholder="Enter username" value={value} onChange={onChange}/>
                 <input type="submit" name="submit" value="Search Users"/>
