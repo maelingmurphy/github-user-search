7df12e53ce8c45a325279c065851a51791cdc299
Maeling <maelingmurphy@users.noreply.github.com>
2021-08-13 10:41:51 -0400

    Update userInput state with input field changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
 
 function App() {
+
+  // Set state variable (userInput) to store user search form input value
+  const [userInput, setUserInput] = useState("");
+
+  // Update state based on input field changes
+  const handleChange = (event) => {
+    const userInputValue = event.target.value;
+    setUserInput(userInputValue);
+    console.log("User input:", userInput);
+  }
+
   return (
     <div>
       <Header />
-      <SearchForm />
+      <SearchForm onChange={handleChange} value={userInput} />
+      <h2>{userInput}</h2>
     </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function SearchForm() {
+function SearchForm({onChange, value}) {
     return (
         <div>
             <form>
                 <label htmlFor="username">Enter username</label>
-                <input type="text" id="username" placeholder="Enter username"/>
+                <input type="text" id="username" placeholder="Enter username" value={value} onChange={onChange}/>
                 <input type="submit" name="submit" value="Search Users"/>
             </form>           
         </div>
