24fe40e154eb60eac01477a0ece642bb0273abfa
Maeling <maelingmurphy@users.noreply.github.com>
2021-08-16 09:06:18 -0400

    Style SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./css/SearchForm.css";
 
 function SearchForm({onChange, value, onSubmit}) {
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="username">Enter username</label>
-                <input type="text" id="username" placeholder="Enter username" value={value} onChange={onChange} required/>
-                <input type="submit" name="submit" value="Search Users"/>
+        <section className="search">
+            <form className="search__form" onSubmit={onSubmit}>
+                <label className="search__label" htmlFor="username">Enter username</label>
+                <input className="search__input" type="text" id="username" placeholder="Enter username" value={value} onChange={onChange} required/>
+                <input className="search__btn" type="submit" name="submit" value="Search Users"/>
             </form>           
-        </div>
+        </section>
     );
 }
 
